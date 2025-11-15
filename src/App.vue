7bd9faeb4f06a378c68f6ea2c305c0ec9c585944
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2025-11-15 13:06:30 +0000

    fix case-sensitive import and add TypeScript shim for bootstrap-vue-3

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
   </div>
 </template>
 
-<script>
-import Navbar from './components/Navbar.vue'
+<script lang="ts">
+import Navbar from './components/NavBar.vue'
 
 export default {
   components: {
