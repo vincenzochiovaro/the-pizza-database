c78b5ba89049ffe2c6c8a89064a23709f5bef25a
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2025-11-16 20:16:35 +0000

    Add bootstrap/scaffolding

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,71 +1,71 @@
 <template>
   <div class="container-fluid p-0" style="font-family: Palatino, serif;">
     <!-- Navbar -->
-    <b-navbar type="light" variant="light" class="py-3 px-2" style="background-color: #fdf6e3;">
-      <b-navbar-brand href="#">
+    <nav class="navbar navbar-light py-3 px-2" style="background-color: #fdf6e3;">
+      <a class="navbar-brand" href="#">
         <img src="" alt="Left Logo" class="img-fluid" style="height: 40px;">
-      </b-navbar-brand>
+      </a>
 
       <span class="h4 text-dark mx-3 m-0">EV Database</span>
 
-      <b-navbar-brand href="#">
+      <a class="navbar-brand" href="#">
         <img src="" alt="Right Logo" class="img-fluid" style="height: 40px;">
-      </b-navbar-brand>
-    </b-navbar>
+      </a>
+    </nav>
 
     <!-- Filters area -->
     <div class="d-flex flex-wrap justify-content-start gap-2 py-3 px-2 bg-white">
-      <b-form-select
-        v-model="selectedBrand"
-        :options="brandOptions"
-        class="w-auto text-dark"
-      ></b-form-select>
+      <select v-model="selectedBrand" class="form-select w-auto text-dark">
+        <option v-for="opt in brandOptions" :key="opt.value" :value="opt.value">
+          {{ opt.text }}
+        </option>
+      </select>
 
-      <b-form-select
-        v-model="selectedRange"
-        :options="rangeOptions"
-        class="w-auto text-dark"
-      ></b-form-select>
+      <select v-model="selectedRange" class="form-select w-auto text-dark">
+        <option v-for="opt in rangeOptions" :key="opt.value" :value="opt.value">
+          {{ opt.text }}
+        </option>
+      </select>
 
-      <b-form-select
-        v-model="selectedPrice"
-        :options="priceOptions"
-        class="w-auto text-dark"
-      ></b-form-select>
+      <select v-model="selectedPrice" class="form-select w-auto text-dark">
+        <option v-for="opt in priceOptions" :key="opt.value" :value="opt.value">
+          {{ opt.text }}
+        </option>
+      </select>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref } from "vue"
 
 export default defineComponent({
   name: "NavbarWithFilters",
   setup() {
-    const selectedBrand = ref(null);
-    const selectedRange = ref(null);
-    const selectedPrice = ref(null);
+    const selectedBrand = ref(null)
+    const selectedRange = ref(null)
+    const selectedPrice = ref(null)
 
     const brandOptions = [
       { value: null, text: "Brand" },
       { value: 1, text: "Tesla" },
       { value: 2, text: "Nissan" },
       { value: 3, text: "MG" },
-    ];
+    ]
 
     const rangeOptions = [
       { value: null, text: "Range" },
       { value: 1, text: "0-200 km" },
       { value: 2, text: "200-400 km" },
       { value: 3, text: "400+ km" },
-    ];
+    ]
 
     const priceOptions = [
       { value: null, text: "Price" },
       { value: 1, text: "Under $30k" },
       { value: 2, text: "$30k-$50k" },
       { value: 3, text: "Above $50k" },
-    ];
+    ]
 
     return {
       selectedBrand,
@@ -74,7 +74,7 @@ export default defineComponent({
       brandOptions,
       rangeOptions,
       priceOptions,
-    };
+    }
   },
-});
+})
 </script>
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import router from './router/router'
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import BootstrapVue3 from 'bootstrap-vue-3'
 
 const app = createApp(App)
 app.use(router)
-app.use(BootstrapVue3)
 app.mount('#app')
