be76c54a42b234890f34cc8559962add229e4df5
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2025-12-01 20:52:49 +0000

    Draft Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,30 @@
 <template>
-  <nav class="navbar navbar-light px-3 shadow-sm rounded-bottom-4" style="background-color:#fdf6e3;">
+  <nav class="navbar navbar-light px-3 shadow-sm rounded-bottom-5" style="background-color:#8F7265;">
     <div class="container-fluid d-flex justify-content-between align-items-center p-0">
 
+
       <div class="d-flex flex-column align-items-center">
-        <img :src="createImg" alt="Left Logo" class="img-fluid" style="height:45px; object-fit:contain;">
+        <button class="btn p-0 bg-transparent d-flex flex-column align-items-center border rounded-2">
+          <img :src="homeImg" alt="Left Logo" class="img-fluid" style="height:55px; object-fit:contain;">
+        </button>
       </div>
 
-      <span class="h4 text-dark m-0 text-center flex-grow-1 fw-bold fst-italic px-3 py-1  rounded-pill">
+      <span class="h4 text-white m-0 text-center flex-grow-1 fw-bold fst-italic px-3 py-1  rounded-pill">
         The Pizza Database
       </span>
 
       <div class="d-flex flex-column align-items-center">
-        <img :src="thePizzaDbImg" alt="Right Logo" class="img-fluid" style="height:45px; object-fit:contain;">
-        <small class="text-dark mt-1">Create</small>
+        <button class="btn p-0 bg-transparent d-flex flex-column align-items-center border rounded-4">
+          <img :src="createImg" alt="Right Logo" class="img-fluid" style="height:45px; object-fit:contain;">
+          <small class="mt-1 fw-bold" style="color:#FF8C42; font-size:0.85rem;">Create</small>
+        </button>
       </div>
 
     </div>
   </nav>
-  <div class="d-flex flex-wrap justify-content-start gap-2 py-3 px-2 bg-white">
-    <select>
-      <option>option1</option>
-    </select>
-    <select class="form-select w-auto text-dark">
-      <option>option2</option>
-    </select>
-  </div>
 </template>
 
 <script setup lang="ts">
 import createImg from '../assets/createimg.png'
-import thePizzaDbImg from '../assets/thepizzadbimg.png'
+import homeImg from '../assets/homeimg.png'
 </script>
