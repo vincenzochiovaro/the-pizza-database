63aa3d100804a650e21b95e26b6cd710cf9d0b3d
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2026-02-04 22:06:36 +0000

    initialize language from localStorage on app boot

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/router'
+import { useLanguageStore } from './stores/LanguageStore'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
+const pinia = createPinia()
+
 app.use(router)
-app.use(createPinia())
+app.use(pinia)
+
+const languageStore = useLanguageStore()
+const savedLanguage = localStorage.getItem("lang") ?? "en"
+languageStore.currentLanguage = savedLanguage
+
 app.mount('#app')
