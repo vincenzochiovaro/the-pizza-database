48b63e1b374d4625830ade334b18e3c86eeec427
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2026-03-05 16:15:05 +0000

    draft

--- src/views/BuilderBrowser.vue
+++ src/views/BuilderBrowser.vue
@@ -6,12 +6,12 @@
 <script setup lang="ts">
 import BuilderPresets from '../components/builder/BuilderPresets.vue';
 import { KeepItWarm } from '../api/PizzaApi';
-import { GetPresetAsync } from '../api/BuilderApi';
+// import { GetPresetAsync } from '../api/BuilderApi';
 
 
 const handlePresetSelected = (preset: string) => {
     console.log("preset emitted to parent - to pass into the API", preset);
-    const presetToDisplay = GetPresetAsync(preset);
+    // const presetToDisplay = GetPresetAsync(preset);
 };
 
 if (!sessionStorage.getItem('apiWarmed')) {
