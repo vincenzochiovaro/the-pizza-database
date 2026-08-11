d6968f5c923bf8b8da5f78b2bc3fa0e0a724bce7
Vincenzo Chiovaro <66358836+vincenzochiovaro@users.noreply.github.com>
2026-08-11 19:11:58 +0100

    init homepage (#32)

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PizzaBrowser from '../views/PizzaBrowser.vue'
 import BuilderBrowser from '../views/BuilderBrowser.vue'
+import HomePage from '../views/HomePage.vue'
 
 const routes = [
-  { path: '/', component: BuilderBrowser},
+  { path: '/', component: HomePage },
+  { path: '/builder-browser', component: BuilderBrowser },
   { path: '/pizza-browser', component: PizzaBrowser }
 
 ]
